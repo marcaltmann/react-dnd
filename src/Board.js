84b9efefc7b3b8494b297d9790209412b1757d8f
Marc Altmann <marcaltmann@posteo.de>
2017-09-02 22:21:50 +0200

    Finish tutorial

--- src/Board.js
+++ src/Board.js
@@ -1,27 +1,32 @@
 import React, { Component } from 'react';
+import { DragDropContext } from 'react-dnd';
+import HTML5Backend from 'react-dnd-html5-backend';
 import PropTypes from 'prop-types';
-import Square from './Square';
+import BoardSquare from './BoardSquare';
 import Knight from './Knight';
+import { canMoveKnight, moveKnight } from './Game';
 
-export default class Board extends Component {
+class Board extends Component {
   renderSquare(i) {
     const x = i % 8;
     const y = Math.floor(i / 8);
-    const black = (x + y) % 2 === 1;
-
-    const [knightX, knightY] = this.props.knightPosition;
-    const piece = (x === knightX && y === knightY) ?
-      <Knight /> :
-      null;
 
     return (
       <div key={i}
            style={{ width: '12.5%', height: '12.5%' }}>
-        <Square black={black}>
-          {piece}
-        </Square>
+        <BoardSquare x={x}
+                     y={y}>
+          {this.renderPiece(x, y)}
+        </BoardSquare>
       </div>
     );
+  }
+
+  renderPiece(x, y) {
+    const [knightX, knightY] = this.props.knightPosition;
+    if (x === knightX && y === knightY) {
+      return <Knight />;
+    }
   }
 
   render() {
@@ -48,3 +53,5 @@ Board.propTypes = {
     PropTypes.number.isRequired
   ).isRequired,
 };
+
+export default DragDropContext(HTML5Backend)(Board);
